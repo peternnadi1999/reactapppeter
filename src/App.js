add422086709c66195aff3ecd4437b6b5a6de160
nnadi peter <peternnadi1999@gmail.com>
2022-02-17 15:44:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import classCom from './Component/classCom';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <classCom />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Component/classCom.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+export class classCom extends React.Component {
+
+    render(){
+        return (
+            <p>this is the function page</p>
+        );
+    }
+   
+}
